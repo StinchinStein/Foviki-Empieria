039954e76a9643f891981494cbf48504e5e62514
BJ Rafferty <bjrafferty2000@gmail.com>
2019-02-03 19:29:46 -0500

    Update
    + Added Spawn Position Icon
    * Fixed FPSController
    * Fixed Asylum issues

---
+++ Assets/SpawnPosIcon.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SpawnPosIcon : MonoBehaviour {
+
+    void Start() {
+        
+    }
+    void OnDrawGizmos() {
+        Gizmos.DrawIcon(transform.position, "SpawnIcon.png", true);
+    }
+    void Update() {
+        
+    }
+}
