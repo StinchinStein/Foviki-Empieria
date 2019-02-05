c285adcd555a1f7839a49af9aaa898f4dc927694
BJ Rafferty <bjrafferty2000@gmail.com>
2019-02-04 23:43:03 -0500

    Door stuff

---
+++ Assets/Resources/DoorScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour {
+
+    private Animator anim;
+    public bool isOpen;
+    void Start() {
+        anim = GetComponent<Animator>();
+    }
+    
+    void Update() {
+        anim.SetBool("IsOpen", isOpen);
+
+    }
+}
